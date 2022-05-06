8e3ac600d7884cfd8b794a276f976d20910f06ca
abacco <alvise.bacco@gmail.com>
2022-05-06 18:30:35 +0200

    inserimento oggetti grafici in ridefinizione override

--- Client/DrawMainInterface.py
+++ Client/DrawMainInterface.py
@@ -4,13 +4,14 @@ from tkinter import messagebox
 
 class DrawMainObj:
     def __init__(self, window_bg, title, geometry):
-
         self.window = tk.Tk()
         self.window.geometry(geometry)
         self.window['background'] = window_bg
         self.window.title(title)
 
-    def draw_title(self, name: str, surname: str, role: str, dimension: int, bg: str, fg: str, x: int, y: int):
+    def draw_title(self, name: str, surname: str, role: str,
+                   dimension: int, dimension_sub: int,
+                   bg: str, fg: str, x: int, y: int):
         label_welcome = tk.Label(self.window,
                                  text=f'Benvenuto {role} {name} {surname}',
                                  bg=bg,
@@ -22,20 +23,91 @@ class DrawMainObj:
                                   text=f'Segnalazioni di reazioni avverse da vaccini anti-Covid',
                                   bg=bg,
                                   fg=fg,
-                                  font=('Courier', dimension, 'bold'))
-        label_subtitle.place(x=x, y=y+20)
+                                  font=('Courier', dimension_sub, 'bold'))
+        label_subtitle.place(x=x, y=y + 60)
+
+    # Istanzia label
+    def label_patient_informations(self, label_name, text, fg, bg, x, y):
+        label_name = tk.Label(self.window,
+                              text=text,
+                              fg=fg,
+                              bg=bg,
+                              font=('Courier', 16, 'bold'))
+        label_name.place(x=x, y=y)
+
+    def separator(self, separator: str, bg: str, h: int, w: int, x: int, y: int):
+        separator = tk.Frame(self.window, bg=bg, height=h, width=w)
+        separator.place(x=x, y=y)
 
 
 class DrawDoctor(DrawMainObj):
     def __init__(self):
-        self.dark_red: str = '#910000'
+        self.under_pressure = None
+        self.fatty_boy = None
+        self.heart_cancer = None
+        self.smoke = None
+
+        self.dark_red: str = '#3c002f'
         self.solarized_red: str = '#FF8282'
+
         super(DrawDoctor, self).__init__(window_bg=self.dark_red, title='CSET Edizione Medico',
                                          geometry='960x768')
 
     def draw_title(self, name: str, surname: str, **kwargs) -> None:
         super(DrawDoctor, self).draw_title(name, surname,
-                                           'Dr.', 36, self.dark_red, self.solarized_red, 100, 10)
+                                           'Dr.', 36, 16, self.dark_red, self.solarized_red, 100, 10)
+        self.get_patient_information()
+
+    def get_patient_information(self, **kwargs):
+        super(DrawDoctor, self).label_patient_informations('label_year_of_birth', 'Anno di nascita:',
+                                                           self.solarized_red, self.dark_red, 10, 120)
+        super(DrawDoctor, self).label_patient_informations('label_city', 'Provincia:',
+                                                           self.solarized_red, self.dark_red, 10, 150)
+        super(DrawDoctor, self).label_patient_informations('job', 'Professione:',
+                                                           self.solarized_red, self.dark_red, 10, 180)
+        super(DrawDoctor, self).label_patient_informations('risk_factor', 'Fattori di rischio',
+                                                           self.solarized_red, self.dark_red, 10, 270)
+
+        tb_yyyy = tk.Entry(self.window,
+                           bg=self.solarized_red,
+                           fg=self.dark_red,
+                           font=('Courier', 16, 'bold'))
+        tb_yyyy.place(x=500, y=120)
+
+        tb_city = tk.Entry(self.window,
+                           bg=self.solarized_red,
+                           fg=self.dark_red,
+                           font=('Courier', 16, 'bold'))
+        tb_city.place(x=500, y=150)
+
+        tb_job = tk.Entry(self.window,
+                          bg=self.solarized_red,
+                          fg=self.dark_red,
+                          font=('Courier', 16, 'bold'))
+        tb_job.place(x=500, y=180)
+
+        super(DrawDoctor, self).separator('first', self.solarized_red, 2, 950, 10, 240)
+
+        # CheckBox
+        ck_smoke = tk.Checkbutton(self.window, bg=self.dark_red, fg=self.solarized_red, text='Fumo',
+                                  variable=self.smoke, onvalue=1, offvalue=0,
+                                  font=('Courier', 16, 'bold'))
+        ck_smoke.place(x=100, y=300)
+
+        ck_smoke = tk.Checkbutton(self.window, bg=self.dark_red, fg=self.solarized_red, text='Ipertensione',
+                                  variable=self.under_pressure, onvalue=1, offvalue=0,
+                                  font=('Courier', 16, 'bold'))
+        ck_smoke.place(x=300, y=300)
+
+        ck_smoke = tk.Checkbutton(self.window, bg=self.dark_red, fg=self.solarized_red, text='Sovrappeso',
+                                  variable=self.fatty_boy, onvalue=1, offvalue=0,
+                                  font=('Courier', 16, 'bold'))
+        ck_smoke.place(x=500, y=300)
+
+        ck_smoke = tk.Checkbutton(self.window, bg=self.dark_red, fg=self.solarized_red, text='Cardio/Onco',
+                                  variable=self.heart_cancer, onvalue=1, offvalue=0,
+                                  font=('Courier', 16, 'bold'))
+        ck_smoke.place(x=700, y=300)
 
 
 class DrawPharmaMan(DrawMainObj):
@@ -47,4 +119,4 @@ class DrawPharmaMan(DrawMainObj):
 
     def draw_title(self, name: str, surname: str, **kwargs) -> None:
         super(DrawPharmaMan, self).draw_title(name, surname,
-                                              'Farm.', 16, self.dark_green, self.solarized_green, 10, 10)
+                                              'Farm.', 16, 16, self.dark_green, self.solarized_green, 10, 10)
--- Server/manager/operator.py
+++ Server/manager/operator.py
@@ -26,31 +26,131 @@ class DatabaseOperations:
                 self.database = config.get(db_name, 'database')
                 self.host = config.get(db_name, 'host')
                 self.port = config.get(db_name, 'port')
-                print(self.username)
         except Exception as e:
             print(Fore.RED + '[ERROR] ' + str(e))
 
         try:
             self.connection = psycopg2.connect('host=' + self.host + ' '
-                                               'port=' + self.port + ' '
-                                               'dbname=' + self.database + ' '
-                                               'user=' + self.username + ' '
-                                               'password=' + self.password)
+                                                                     'port=' + self.port + ' '
+                                                                                           'dbname=' + self.database + ' '
+                                                                                                                       'user=' + self.username + ' '
+                                                                                                                                                 'password=' + self.password)
 
             print(Fore.GREEN + '[+] Connected to database: ', self.database)
+            self.cursor = self.connection.cursor()
         except (Exception, psycopg2.DatabaseError) as e:
             print(Fore.RED + str(e))
 
-    async def check_and_create_tables(self):
+    async def check_and_create_table_reaction(self):
+        """tabella di reazione"""
+        table_reaction = 'reaction'
+        column_severity = 'severity'
+        column_description = 'description'
+        column_reporting = 'reporting'
+
+        sql = "CREATE TABLE IF NOT EXISTS %s " \
+              "(%s varchar(50) PRIMARY KEY, " \
+              "%s varchar(50) NOT NULL, " \
+              "%s varchar(50) NOT NULL)" % (table_reaction, column_severity, column_description, column_reporting)
+
+        await self.create_table(sql)
+
+    async def check_and_create_table_reporting(self):
+        """ tabella di inserimento dei dati principali"""
+
+        table_reporting = 'reporting'
+        column_id = 'id_'
+        column_patient = 'patient'
+        column_adverse_reaction = 'adv_reaction'
+        column_date_of_reaction = 'reaction_date'
+        column_date_of_reporting = 'reporting_date'
+        column_vaccination_carried_out = 'vaccination_carried_out'
+
+        sql = "CREATE TABLE IF NOT EXISTS %s " \
+              "(%s varchar(50) PRIMARY KEY, " \
+              "%s varchar(50) NOT NULL, " \
+              "%s varchar(50) NOT NULL, " \
+              "%s varchar(50) NOT NULL, " \
+              "%s varchar(50) NOT NULL, " \
+              "%s varchar(50) NOT NULL)" % (table_reporting, column_id, column_patient, column_adverse_reaction,
+                                            column_date_of_reaction, column_date_of_reporting,
+                                            column_vaccination_carried_out)
+        await self.create_table(sql)
+
+    async def check_and_create_table_risk(self):
+        """tabella con i dati del rischio"""
+        table_user = 'Risk'
+
+        column_id = 'id_'
+        column_description = 'Description'
+        column_risk_level = 'Risk_level'
+
+        sql = "CREATE TABLE IF NOT EXISTS %s " \
+              "(%s varchar(50) PRIMARY KEY, " \
+              "%s varchar(50) NOT NULL, " \
+              "%s varchar(50) NOT NULL)" % (table_user, column_id, column_description, column_risk_level)
+        await self.create_table(sql)
+
+    async def check_and_create_table_vaccination(self):
+        """tabella con i dati dei vaccini"""
+        table_vaccination = 'vaccination'
+        column_id = 'id_'
+        column_patient = 'patient'
+        column_reporting = 'reporting'
+        column_vaccination = 'vaccination'
+        column_dose = 'dose'
+        column_site = 'site'
+        column_vaccination_date = 'vacciantion_date'
+
+        sql = "CREATE TABLE IF NOT EXISTS %s " \
+              "(%s varchar(50) PRIMARY KEY, " \
+              "%s varchar(50) NOT NULL, " \
+              "%s varchar(50) NOT NULL, " \
+              "%s varchar(50) NOT NULL, " \
+              "%s varchar(50) NOT NULL, " \
+              "%s varchar(50) NOT NULL, " \
+              "%s varchar(50) NOT NULL)" % (table_vaccination, column_id, column_patient,
+                                            column_reporting, column_vaccination,
+                                            column_dose, column_site, column_vaccination_date)
+
+        await self.create_table(sql)
+
+    async def check_and_create_table_patient(self):
+        """tabella con i dati del paziente"""
+
+        table_patient = 'patient'
+        column_id = 'id_'
+        column_year_of_birth = 'date_of_birth'
+        column_province_of_residence = 'province_residence'
+        column_job = 'job'
+        column_risk_factor = 'risk_factor'
+        column_previous_vaccinations = 'previous_vaccination'
+        column_doctor = 'doctor'
+
+        sql = "CREATE TABLE IF NOT EXISTS %s " \
+              "(%s varchar(50) NOT NULL, " \
+              "%s varchar(50) NOT NULL, " \
+              "%s varchar(50) NOT NULL, " \
+              "%s varchar(50) NOT NULL, " \
+              "%s varchar(50) NOT NULL, " \
+              "%s varchar(50) NOT NULL, " \
+              "%s varchar(50) PRIMARY KEY, " \
+              "foreign key(doctor) REFERENCES customers(cf));" % (table_patient, column_job, column_risk_factor,
+                                                                  column_previous_vaccinations,
+                                                                  column_year_of_birth,
+                                                                  column_province_of_residence, column_id,
+                                                                  column_doctor)
+        await self.create_table(sql)
+
+    async def check_and_create_table_user(self):
         """ creo le tabelle se non esistono gia' """
 
-        cursor = self.connection.cursor()
-        table_user = r'Customers'
-        column_name = r'Name'
-        column_surname = r'Surname'
-        column_pass = r'Passwd'
-        column_role = r'Role'
-        column_cf = r'CF'
+        table_user = 'customers'
+        column_name = 'Name'
+        column_surname = 'Surname'
+        column_pass = 'Passwd'
+        column_role = 'Role'
+        column_cf = 'CF'
         sql = "CREATE TABLE IF NOT EXISTS %s " \
               "(%s varchar(50) NOT NULL, " \
               "%s varchar(50) NOT NULL, " \
@@ -58,11 +158,7 @@ class DatabaseOperations:
               "%s varchar(128) NOT NULL, " \
               "%s varchar(50) PRIMARY KEY)" % (table_user, column_name, column_surname,
                                                column_role, column_pass, column_cf)
-        try:
-            cursor.execute(sql)
-            self.connection.commit()
-        except (Exception, psycopg2.DatabaseError) as e:
-            print(Fore.RED + str(e))
+        await self.create_table(sql)
 
     def post_new_user(self, new_user_data: json) -> str:
         """ inserisco i dati utente nel db """
@@ -88,11 +184,11 @@ class DatabaseOperations:
         # SQL injection
         sql = "SELECT name, surname, role FROM customers WHERE cf='%s' AND passwd='%s'" % (fiscal_code, password)
         # NO SQL injection
-        #sql = "SELECT name, surname, role FROM customers WHERE cf=%s AND passwd=%s"
+        # sql = "SELECT name, surname, role FROM customers WHERE cf=%s AND passwd=%s"
         cursor = self.connection.cursor()
         try:
             # NO SQL injection!
-            #cursor.execute(sql, (fiscal_code, password))
+            # cursor.execute(sql, (fiscal_code, password))
 
             # YES SQL injection
             cursor.execute(sql)
@@ -129,6 +225,13 @@ class DatabaseOperations:
         except Exception as e:
             print(e)
             return False
+
+    async def create_table(self, sql):
+        try:
+            self.cursor.execute(sql)
+            self.connection.commit()
+        except (Exception, psycopg2.DatabaseError) as e:
+            print(Fore.RED + str(e))
 
     @staticmethod
     def get_password_hash(password: str) -> str:
